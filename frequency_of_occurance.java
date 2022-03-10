f05ee7e0c81658f4c6b92032d29beb8b625012f8
v-rane <101250706+v-rane@users.noreply.github.com>
2022-03-10 20:59:09 +0530

    Update frequency_of_occurance.java

--- frequency_of_occurance.java
+++ frequency_of_occurance.java
@@ -3,13 +3,20 @@ import java.util.*;
 
 public class frequency_of_occurance {
 	public static void main(String[]args) {
-		  int c=0;
+		  
 		Scanner sc= new Scanner(System.in);
 		System.out.println("enter a string");
 		String str = sc.nextLine();
 		System.out.println("enter a word");
 		String word= sc.next();
+		frequency_of_occurance f = new frequency_of_occurance();
+		f.freq(str, word);
+	}
+		public void freq(String str,String word) {
+			int c =0;
+		
 		String[] a= str.split(" ");
+	
 		for(int i=0;i<a.length;i++)
 		     if(a[i].equals(word))
 		    	 c++;
