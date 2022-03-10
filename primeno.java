42561ab3564e47c34003481dff8bcd20cbe4c3fd
v-rane <101250706+v-rane@users.noreply.github.com>
2022-03-10 21:03:11 +0530

    Update primeno.java

--- primeno.java
+++ primeno.java
@@ -1,29 +1,42 @@
 package sebone;
 import java.util.*;
 public class primeno {
-
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("enter a num");
-		int maxnum = scan.nextInt();
-		scan.close();
-		System.out.println("list of prime no. are:");
-		for (int num =2;num <=maxnum;num++) {
-			boolean isprime =true;
-			for(int i=2;i<=num/2;i++) {
-				if(num%i==0) {
-					isprime =false;
-					break;
+	    public void prime(int no) {
+	    	for (int num =2;num <=no;
+	    			num++) {
+				boolean isprime =true;
+				for(int i=2;i<=num/2;i++) {
+					if(num%i==0) {
+						isprime =false;
+						break;
+					}
 				}
+				if(isprime==true) {
+					System.out.println(num);
+				}
+				
 			}
-			if(isprime==true) {
-				System.out.println(num);
-			}
+	    }
+	
+  
+	
+		
+		public static void main(String args[]) {
+           
+			Scanner scan = new Scanner(System.in);
+			System.out.println("enter a num");
+			int maxnum = scan.nextInt();
+			System.out.println("list of prime no. are:");
+			primeno p=new primeno();
+			p.prime(maxnum);
+			scan.close();
 			
 		}
-		
-		
 	
 	}
 
-}
+
+
+
+
+
