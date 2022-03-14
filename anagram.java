73d76dd5ee5312287b89c3719d483ac72a9ff9c5
v-rane <101250706+v-rane@users.noreply.github.com>
2022-03-15 00:56:46 +0530

    Update anagram.java

--- anagram.java
+++ anagram.java
@@ -1,9 +1,11 @@
+
+/*program to print two strings are anagram or not i.e. can form one string  by arranging the char of another string */
+
 package sebone;
 import java.util.*;
-/*program to print two strings are anagram or not i.e. can form one string  by arranging the char of another string */
 public class anagram {
 	public static void main(String[]args) {
-		Scanner sc= new Scanner(System.in); //to take input
+		Scanner sc= new Scanner(System.in); // to take input
 		System.out.println("enter string1 ");
 		String str1= sc.nextLine();
 		System.out.println("enter string2");
@@ -13,11 +15,11 @@ public class anagram {
 	}
 		public void ana(String str1,String str2) {
 			
-	char a[]=str1.toLowerCase().toCharArray(); // toCharArray() will convert the string into char array
+	char a[]=str1.toLowerCase().toCharArray();  // toCharArray() will convert the string into char array
 		char b[] = str2.toLowerCase().toCharArray();
-		Arrays.sort(a);//for sorting char array
+		Arrays.sort(a);//to sort array
 		Arrays.sort(b);
-		boolean result =Arrays.equals(a,b);// check sorted array are equals
+		boolean result =Arrays.equals(a,b); // check sorted array are equals
 		if(result==true) {
 			System.out.println("Strings are anagram");
 			
