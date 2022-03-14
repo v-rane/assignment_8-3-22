38c13d25f894d603ea5efc43f0b04fad2f44a47d
v-rane <101250706+v-rane@users.noreply.github.com>
2022-03-15 01:03:47 +0530

    Update frequency_of_occurance.java

--- frequency_of_occurance.java
+++ frequency_of_occurance.java
@@ -1,10 +1,10 @@
 package sebone;
 import java.util.*;
-
+/* to find the frequency of occurrence of a word in an article*/
 public class frequency_of_occurance {
 	public static void main(String[]args) {
 		  
-		Scanner sc= new Scanner(System.in);
+		Scanner sc= new Scanner(System.in); // to take input
 		System.out.println("enter a string");
 		String str = sc.nextLine();
 		System.out.println("enter a word");
@@ -13,12 +13,12 @@ public class frequency_of_occurance {
 		f.freq(str, word);
 	}
 		public void freq(String str,String word) {
-			int c =0;
+			int c =0; 
 		
-		String[] a= str.split(" ");
+		String[] a= str.split(" ");// to split a string into an array of substrings
 	
 		for(int i=0;i<a.length;i++)
-		     if(a[i].equals(word))
+		     if(a[i].equals(word))//compare two given string
 		    	 c++;
 		System.out.println(c);
 	}
